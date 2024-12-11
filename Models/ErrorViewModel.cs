0ee53b3f083d1523fa14358d9c03c86a87b0905b
David <ddiz@neo-ds.es>
2024-12-11 15:28:44 +0100

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaOnline.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
